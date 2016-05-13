6d944b94fa42ac5847fba2b8239682cc108d360c
Jean Leong <jeanleong98@gmail.com>
2016-05-13 10:24:03 -0400

    fixed typo

--- app/src/main/java/com/example/jeanleong/testing510/MainActivity.java
+++ app/src/main/java/com/example/jeanleong/testing510/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent addTask = new Intent(MainActivity.this, LogTaskActivity.class);
+                Intent addTask = new Intent(MainActivity.this, AddTaskActivity.class);
                 startActivity(addTask);
             }
         });
