c4e8107938523a9fcc53d23ecdb1b721d2e74004
Jean Leong <jeanleong98@gmail.com>
2016-05-13 10:38:39 -0400

    added task screen

--- app/src/main/java/com/example/jeanleong/testing510/AddTaskActivity.java
+++ app/src/main/java/com/example/jeanleong/testing510/AddTaskActivity.java
@@ -2,8 +2,14 @@ package com.example.jeanleong.testing510;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class AddTaskActivity extends AppCompatActivity {
+    Spinner courseList;
+    TextView assignment;
+    Button dueDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
